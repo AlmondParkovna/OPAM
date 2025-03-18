aa129a3dc88cb9fcc2bd03fdaad9ad2cd23c845c
Владислав Буй <topgoodnice@gmail.com>
2025-03-18 11:31:59 +0200

    Додано CI

---
+++ src/data.hpp
@@ -0,0 +1,9 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <string>
+
+const std::string firstName = "Vlad";
+const std::string lastName = "Buy";
+
+#endif // DATA_HPP
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "data.hpp"
+
+int main() {
+    std::cout << firstName << " " << lastName << std::endl;
+    return 0;
+}
